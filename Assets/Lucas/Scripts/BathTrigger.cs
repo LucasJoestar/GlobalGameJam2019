eaba6b3707ee3e9c43fdb98a9793ef254b6e6bf4
Luco <lucasjoestar@outlook.fr>
2019-01-27 13:02:05 +0100

    OK

---
+++ Assets/Lucas/Scripts/BathTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathTrigger : MonoBehaviour
+{
+    public void EndBathRiddle()
+    {
+        GameManager.Instance.BathRiddleResolve();
+    }
+}
--- Assets/Lucas/Scripts/GameManager.cs
+++ Assets/Lucas/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
@@ -34,12 +35,18 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject doorsRidleSign = null;
     [SerializeField] private Animator doorsRidleAnim = null;
 
+    [Header("Bath Riddle")]
+    [SerializeField] private GameObject bathRiddleAnchor = null;
+    [SerializeField] private GameObject bathRiddleSign = null;
+    [SerializeField] private Animator bathRiddleAnim = null;
+
     [Header("State")]
     [SerializeField] private bool isEventActive = false;
     [SerializeField] private bool isGameOver = false;
     [SerializeField] private bool isAtDoorsRiddle = false;
     [SerializeField] private bool isAtBathRiddle = false;
     [SerializeField] private bool isResolvingDoorsRiddle = false;
+    [SerializeField] private bool isResolvingBathRiddle = false;
 
     [Header("Time")]
     [SerializeField] private float timer = 0;
@@ -92,6 +99,34 @@ public class GameManager : MonoBehaviour
     #region Methods
 
     #region Original Methods
+    private void BathRiddle(bool _doInput)
+    {
+        if (!_doInput || !isAtBathRiddle || isResolvingBathRiddle || isGameOver) return;
+
+        isResolvingBathRiddle = true;
+
+        bathRiddleSign.gameObject.SetActive(false);
+        bathRiddleAnim.SetTrigger("Play");
+
+        SoundManager.Instance.PlaySuccessFeedback();
+        SoundManager.Instance.PlayBathResolveSound();
+    }
+
+    public void BathRiddleResolve()
+    {
+        isResolvingBathRiddle = false;
+        isAtBathRiddle = false;
+        Destroy(bathRiddleAnchor.gameObject);
+    }
+
+    private void BathRiddleStart()
+    {
+        isAtBathRiddle = true;
+        bathRiddleAnchor.SetActive(true);
+
+        SoundManager.Instance.PlayBathAmbiance();
+    }
+
     /// <summary>
     /// Checks the entered solution by the players
     /// </summary>
@@ -174,6 +209,19 @@ public class GameManager : MonoBehaviour
         isGameOver = true;
 
         OnGameEnd?.Invoke(_isSuccess);
+
+        if (_isSuccess) Invoke("LoadGoodEnd", 2);
+        else Invoke("LoadBadEnd", 2);
+    }
+
+    private void LoadBadEnd()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void LoadGoodEnd()
+    {
+        SceneManager.LoadScene(3);
     }
 
     /// <summary>
@@ -229,6 +277,12 @@ public class GameManager : MonoBehaviour
         // Update the event timer
         EventTimer += Time.deltaTime;
 
+        if (eventTimer >= bathRiddleStartTime && bathRiddleAnchor != null)
+        {
+            BathRiddleStart();
+            return;
+        }
+
         if (eventTimer >= eventTimeLimit)
         {
             StartEvent();
@@ -247,6 +301,12 @@ public class GameManager : MonoBehaviour
         }
 
         GloveInputsManager.OnFifthCombination += DoorsRiddle;
+        InputsManager.OnRightBumperDownInputPress += DoorsRiddle;
+        InputsManager.OnKBAFiveDownInputPress += DoorsRiddle;
+
+        GloveInputsManager.OnSixCombination += (float _value) => BathRiddle(_value > 0.5f);
+        InputsManager.OnLeftBumperDownInputPress += BathRiddle;
+        InputsManager.OnKBASixDownInputPress += BathRiddle;
     }
 
     private void FixedUpdate()
